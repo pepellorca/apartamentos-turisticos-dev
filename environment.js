f281619ef9e4339a30e3c4706845bd7874298f86
pepellorca <pepe.llorca@gmail.com>
2026-09-24 13:48:35 +0200

    Update environment.js

--- environment.js
+++ environment.js
@@ -1,2 +1,4 @@
 // Configuración pública del entorno. Generar con tools/configure_environment.py. Nunca poner secretos aquí.
-window.BND_ENV=Object.freeze({appsScriptUrl:""});
+window.BND_ENV=Object.freeze({
+  appsScriptUrl:"https://script.google.com/macros/s/AKfycbzZ_qs2K_L0wENkeTk6BY57oylwcnV_anevnjUghT9VbmNdwP-kBjsazEkq-kwQcW2z/exec"
+});
