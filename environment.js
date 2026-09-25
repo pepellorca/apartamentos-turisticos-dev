3b04fff2845d14bd867a6b2b85455aa349173858
pepellorca <pepe.llorca@gmail.com>
2026-09-25 13:35:49 +0200

    Add files via upload

--- environment.js
+++ environment.js
@@ -1,4 +1,2 @@
 // Configuración pública del entorno. Generar con tools/configure_environment.py. Nunca poner secretos aquí.
-window.BND_ENV=Object.freeze({
-  appsScriptUrl:"https://script.google.com/macros/s/AKfycbzZ_qs2K_L0wENkeTk6BY57oylwcnV_anevnjUghT9VbmNdwP-kBjsazEkq-kwQcW2z/exec"
-});
+window.BND_ENV=Object.freeze({appsScriptUrl:""});
